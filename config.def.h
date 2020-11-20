a962d30b408d3bde6e4d21da27c565d7c7e73073
Luca Wellmeier <luca_wellmeier@gmx.de>
2020-11-20 21:25:51 +0100

    config

--- config.def.h
+++ config.def.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
-static const char *browsercmd[] = { "surf -h" };
+static const char *browsercmd[] = { "surf", "-h" };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
